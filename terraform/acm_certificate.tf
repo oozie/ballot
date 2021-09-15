30cee52dfc6d132324a84d596a6cb774f254a1d9
Oozie Ligus <root@ooz.ie>
2021-09-14 21:28:07 -0700

    add validation

--- terraform/acm_certificate.tf
+++ terraform/acm_certificate.tf
@@ -7,3 +7,8 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.domain.name]
+}
+
