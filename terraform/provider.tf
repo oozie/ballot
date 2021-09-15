8414d09a624ac4877bb0ae019b0bbf29096d9a09
Oozie Ligus <root@ooz.ie>
2021-09-14 22:32:03 -0700

    change provider region

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,4 @@
 provider "aws" {
   profile    = "ballot"
-  region     = "us-west-1"
+  region     = "us-east-1"
 }
-
-provider "aws" {
-  # us-east-1 instance
-  region = "us-east-1"
-  alias = "us-east-1"
-}
-
