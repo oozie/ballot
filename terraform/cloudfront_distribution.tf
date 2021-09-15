0cd70ca077596270865b7f5df356fe7319d85089
Oozie Ligus <root@ooz.ie>
2021-09-14 21:18:34 -0700

    custom origin config

--- terraform/cloudfront_distribution.tf
+++ terraform/cloudfront_distribution.tf
@@ -2,6 +2,12 @@ resource "aws_cloudfront_distribution" "cdn" {
   origin {
     origin_id = var.domain
     domain_name = aws_s3_bucket.ballot.website_endpoint
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
   }
 
   # If using route53 aliases for DNS we need to declare it here too, otherwise we'll get 403s.
@@ -9,6 +15,7 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   enabled             = true
   default_root_object = "index.html"
+
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
