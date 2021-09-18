b5a0fb88a5f8af67726f10ddd33de3427d1eb1e4
Oozie Ligus <root@ooz.ie>
2021-09-18 15:42:20 -0700

    Add material UI appbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
 import { useEffect, useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import { InjectedConnector } from '@web3-react/injected-connector'
 
 const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42],
 })
+
+
+function ButtonAppBar(props) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Blockchain Ballot
+          </Typography>
+          <Button color="inherit" onClick={ props.active ? () => {} : props.connect }>
+            { props.active ? props.account : "Connect wallet" }
+        </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
 
 
 function App() {
@@ -43,18 +68,8 @@ function App() {
 
   return (
     <div className="App">
+      <ButtonAppBar active={active} connect={connect} disconnect={disconnect} account={account} />
       <header className="App-header">
-
-        <p>
-          Account: { account }
-        </p>
-        <p>
-          Authorized: { authorized ? "YES" : "NO" }
-        </p>
-
-        <button onClick={ active ? disconnect : connect }>
-          { active ? "Disconnect" : "Connect" }
-        </button>
 
       </header>
     </div>
